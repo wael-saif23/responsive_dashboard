3f256b34f5f329728c99ad28260275fe7fd342fd
wael-saif23 <ws.mywork@gmail.com>
2024-05-20 21:22:09 +0300

    Refactor AllExpensesHeader widget
    This commit refactors the AllExpensesHeader widget by adding a container with a dropdown button for selecting monthly expenses. It also adds styling to the text and container.

--- lib/views/widgets/all_expenses_header.dart
+++ lib/views/widgets/all_expenses_header.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
 
 class AllExpensesHeader extends StatelessWidget {
   const AllExpensesHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 16),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 16),
       child: Row(
-        
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "All Expenses",
+            style: AppStyles.styleSemiBold20,
+          ),
+          Container(
+            decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: const BorderSide(
+                      color: Color(0xffF1F1F1),
+                      width: 1,
+                    ))),
+            child: const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Monthly",
+                    style: AppStyles.styleMedium16,
+                  ),
+                  Icon(Icons.keyboard_arrow_down),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
