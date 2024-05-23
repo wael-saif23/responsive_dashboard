59c934e3ff973d9999c48121875e09ab72241f80
wael-saif23 <ws.mywork@gmail.com>
2024-05-23 22:49:41 +0300

    Add responsive custom drawer and menu button to dashboard view

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/adaptive_layouts.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/desktop_dashboard_layout.dart';
 import 'package:responsive_dashboard/views/widgets/mobile_dashbourd_layout.dart';
 import 'package:responsive_dashboard/views/widgets/tablet_dashbourd_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer:
+          MediaQuery.of(context).size.width < 800 ? const CustomDrawer() : null,
       appBar: MediaQuery.of(context).size.width < 800
           ? AppBar(
-            elevation: 0,
+              elevation: 0,
               backgroundColor: const Color(0xffE5E5E5),
-              leading: const Icon(Icons.menu),
-            )
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(Icons.menu),
+              ))
           : null,
       backgroundColor: const Color(0xffE5E5E5),
       body: AdaptiveLayout(
-          mobileLayout: (context) => const MobileDashbourdLayout(),
-          tabletLayout: (context) => const DashBoardTabletLayout(),
-          desktopLayout: (context) => const DashBoardDesktopLayout()),
+        mobileLayout: (context) => const MobileDashbourdLayout(),
+        tabletLayout: (context) => const DashBoardTabletLayout(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
     );
   }
 }
--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -12,6 +12,7 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width:  MediaQuery.of(context).size.width *.7,
       color:  Colors.white,
       child: const CustomScrollView(
         
